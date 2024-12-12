f4385475ba64d61dbadf591f7f81318eeaae03e6
Dorislin01 <81360006t@gmail.com>
2024-12-12 10:13:04 +0800

    1212mornin

--- Assets/Scripts/HuggingFaceManager.cs
+++ Assets/Scripts/HuggingFaceManager.cs
@@ -5,6 +5,8 @@ using Newtonsoft.Json;
 using System;
 using System.Linq;
 using System.Text;
+using System.Collections;
+using System.Collections.Generic;
 
 
 namespace Doris01
@@ -19,7 +21,7 @@ namespace Doris01
 
         private string key = "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";
         private string model = "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L12-v2";
-        
+
         private TMP_InputField inputFieldPlayer;
 
 
@@ -28,61 +30,61 @@ namespace Doris01
 
         private void Awake()
         {
-            inputFieldPlayer = GameObject.Find("輸入欄位1").GetComponent<TMP_InputField>();
+            inputFieldPlayer = GameObject.Find("輸入欄位").GetComponent<TMP_InputField>();
             inputFieldPlayer.onEndEdit.AddListener(PlayerInput);
             npcSentences = npc.data.sentences;
         }
         private void PlayerInput(string input)
-        { 
+        {
             print($"<color=#3f3>玩家輸入 :{input}</color>");
             inputText = input;
             StartCoroutine(GetSimilarity());
         }
-    }
-}
-            private IEnumerator GetSimilarity()
-{
-    var requestData = new
-    {
-        inputs = new
+
+        private IEnumerator GetSimilarity()
         {
-            source_sentence = inputText,
-            sentences = npcSentences
+            var requestData = new
+            {
+                inputs = new
+                {
+                    source_sentence = inputText,
+                    sentences = npcSentences
+                }
+            };
+
+            string jsonBody = JsonConvert.SerializeObject(requestData);
+            byte[] postData = Encoding.UTF8.GetBytes(jsonBody);
+
+            UnityWebRequest request = new UnityWebRequest(model, "POST");
+            request.uploadHandler = new UploadHandlerRaw(postData);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+            request.SetRequestHeader("Authorization", "Bearer " + key);
+
+            yield return request.SendWebRequest();
+
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                print($"<color=#f33>要求失敗:(request.error)</color>");
+            }
+            else
+            {
+                string responseText = request.downloadHandler.text;
+                var response = JsonConvert.DeserializeObject<List<float>>(responseText);
+
+                print($"<color=#3f3>分數:(responseText)</color>");
+
+                if (response != null && response.Count > 0)
+                {
+                    int best = response.Select((value, index) => new
+                    {
+                        Value = value, Index = index
+                    }).OrderByDescending(x => x.Value).First().Index;
+                    print($"<color=#77f>最佳結果:[npcSentences[best]]</color>");
+
+                    npc.PlayAinmation(best);
+                }
+            }
         }
-    };
-
-    string jsonBody = JsonConvert.SerializeObject(requestData);
-    byte[] postData = Encoding.UTF8.GetBytes(jsonBody);
-
-    UnityWebRequest request = new UnityWebRequest(model, "POST");
-    request.uploadHandler = new UploadHandlerRaw(postData);
-    request.downloadHandler = new DownloadHandlerBuffer();
-    request.SetRequestHeader("Content-Type", "application/json");
-    request.SetRequestHeader("Authorization", "Bearer " + key);
-
-    yield return request.SendWebRequest();
-
-    if (request.result != UnityWebRequest.Result.Success)
-    {
-        print($"<color=#f33>要求失敗:(request.error)</color>");
     }
-    else
-    {
-        string responseText = request.downloadHandler.text;
-        var response = JsonConvert.DeserializeObject<List<float>>(responseText);
-
-        print($"<color=#3f3>分數:(responseText)</color>");
-
-        if (response != null && response.Count > 0)
-        {
-            int best = response.Select (value, index) => new
-            Value = value, Index = index
-        子).OrderByDescending(x => x.Value).First().Index;
-            print($"<color=#77f>最佳結果:[npcSentences[best]]</color>");
-
-            npc.PlayAinmation(best);
-        }
-     }
-   }
-  }
 }
